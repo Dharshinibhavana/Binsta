6e31d7b510b961ace643cc10acb0d7456a9c76d3
Dharshinibhavana <dharshinibhavana3@gmail.com>
2025-02-13 13:22:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,275 @@
+document.getElementById("likeBtn1").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn1");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked🖤💓";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+document.getElementById("likeBtn2").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn2");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked🖤🖤";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+
+document.getElementById("likeBtn3").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn3");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked🤍";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+document.getElementById("likeBtn4").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn4");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked💙";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+document.getElementById("likeBtn5").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn5");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked💛🖤";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+document.getElementById("likeBtn6").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn6");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked🖤💛";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+document.getElementById("likeBtn7").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn7");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked💓";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+document.getElementById("likeBtn8").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn8");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked💓";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+document.getElementById("likeBtn9").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn9");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked💓";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+document.getElementById("likeBtn10").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn10");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked💓";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+document.getElementById("likeBtn11").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn11");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked💓";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+document.getElementById("likeBtn12").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn12");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked💓";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+document.getElementById("likeBtn13").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn13");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked💓";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+document.getElementById("likeBtn14").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn14");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked💓";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+document.getElementById("likeBtn15").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn15");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked💓";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+document.getElementById("likeBtn16").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn16");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked💓";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+document.getElementById("likeBtn17").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn17");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked💓";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+document.getElementById("likeBtn18").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn18");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked💓";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+document.getElementById("likeBtn19").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn19");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked💓";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+document.getElementById("likeBtn20").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn20");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked💓";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+document.getElementById("likeBtn21").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn21");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked💓";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+document.getElementById("likeBtn22").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn22");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked💓";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+document.getElementById("likeBtn23").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn23");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked💓";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+document.getElementById("likeBtn24").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn24");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked💓";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
+
+document.getElementById("likeBtn25").addEventListener("click", function() {
+    let btn = document.getElementById("likeBtn25");
+    if (btn.textContent === "Like") {
+        btn.textContent = "Liked💓";
+        btn.style.color = "red";
+    } else {
+        btn.textContent = "Like";
+        btn.style.color = "#0095f6";
+    }
+});
